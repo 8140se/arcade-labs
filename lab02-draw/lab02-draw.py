cb74465615c067a078fe187eded06867630cedb9
8140se <alin.hincu@edu.uah.es>
2023-02-15 23:42:01 +0100

    WIP

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -12,7 +12,17 @@ arcade.draw_lrtb_rectangle_filled(HALF-100, HALF+50, HALF+100, 0, arcade.color.B
 
 arcade.draw_lrtb_rectangle_outline(HALF-100, HALF+50, HALF+100, -5, arcade.color.OLD_GOLD, 3)
 
-arcade.draw_polygon_filled((0, ))
+polygon1 =((HALF+50, 0),
+          (HALF+50, HALF+100),
+          (HALF+90, HALF+147),
+          (HALF+90, 0))
 
+top = ((HALF-100, HALF+100),
+       (HALF+50, HALF+100),
+       (HALF+90, HALF+147),
+       (HALF-60, HALF+147))
+
+arcade.draw_polygon_filled(polygon1, arcade.color.BROWN)
+arcade.draw_polygon_filled(top, arcade.color.BRONZE)
 arcade.finish_render()
 arcade.run()
