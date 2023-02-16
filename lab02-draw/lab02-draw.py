6e0129d927184247d1cf124716b0e83edf7be90f
8140se <124272060+8140se@users.noreply.github.com>
2023-02-16 02:29:03 +0100

    Update lab02-draw.py

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -1,9 +1,9 @@
-"""Hi I'm Renata Bliss and I'm going to be your freestyle dance teacher."""
+"""El siguiente programa dibuja lo único que me vino en mente."""
 import arcade
 
-arcade.open_window(512, 512, "Survey Program")
+arcade.open_window(512, 512, "Whatsapp")
 
-#Fondo
+#Color del fondo
 arcade.set_background_color(arcade.color.PISTACHIO)
 
 arcade.start_render()
@@ -64,39 +64,50 @@ bg_lines = ((0, 0),
             (MAX, 0),
             (MAX, MAX))
 
+#Fondo
 arcade.draw_lines(bg_lines, arcade.color.AMAZON, 110)
+
 #Forma base
 arcade.draw_lrtb_rectangle_filled(HALF-100, HALF+50, HALF+100, 0, (105, 28, 0))
 arcade.draw_polygon_filled(polygon1, (105, 28, 0))
 arcade.draw_polygon_filled(top_triangle, (105, 28, 0))
 arcade.draw_polygon_filled(top_square, (54, 15, 1))
+
 #Líneas que recorren los bordes de la estructura.
 arcade.draw_line_strip(top_triangle_outline, arcade.color.OLD_GOLD, 2.5)
 arcade.draw_lines(middle_outline, arcade.color.OLD_GOLD, 2.5)
 arcade.draw_line_strip(back_outline, arcade.color.OLD_GOLD, 2.5)
+
 #Círculo del reloj
 arcade.draw_circle_filled(HALF-25, HALF+100, 53, (232, 227, 225))
+
 #Ventana de cristal
 arcade.draw_lrtb_rectangle_filled(HALF-70, HALF+25, HALF, -5, (13, 96, 140, 100))
+
 #Péndulo
 arcade.draw_polygon_filled(pendulum, arcade.color.OLD_GOLD)
 arcade.draw_ellipse_filled(HALF+8, HALF-139, 40, 40, arcade.color.OLD_GOLD, 0)
+
 #Rectángulo que "borra" el trozo de péndulo que se sale de la ventana y no pretende ser visto
 arcade.draw_lrtb_rectangle_filled(HALF+25, HALF+30, HALF, 0, (54, 15, 1))
 arcade.draw_lrtb_rectangle_filled(HALF-73, HALF-70, HALF, 0, (54, 15, 1))
 arcade.draw_lrtb_rectangle_filled(HALF-73, HALF+30, HALF+2, HALF, (54, 15, 1))
+
 #Reflejo del cristal
 arcade.draw_polygon_filled(glass_reflection, (87, 147, 186,150))
 arcade.draw_polygon_filled(glass_reflection2, (87, 147, 186,150))
 arcade.draw_polygon_filled(glass_reflection3, (87, 147, 186,150))
+
 #Números del reloj
 arcade.draw_text("XII", HALF-33, HALF+137, arcade.color.BLACK, 10)
 arcade.draw_text("III", HALF+12, HALF+93, arcade.color.BLACK, 10)
 arcade.draw_text("VI", HALF-33, HALF+52, arcade.color.BLACK, 10)
 arcade.draw_text("IX", HALF-73, HALF+93, arcade.color.BLACK, 10)
+
 #Manillares
 arcade.draw_line(HALF-28, HALF+100, HALF+12, HALF+90, arcade.color.BLACK, 3)
 arcade.draw_line(HALF-28, HALF+100, HALF-28, HALF+74, arcade.color.BLACK, 3)
+
 #Circunferencia parcial del reloj
 arcade.draw_arc_outline(HALF-25, HALF+100, 106, 106, (54, 15, 1), 90, 270, 9, 120)
 arcade.finish_render()
