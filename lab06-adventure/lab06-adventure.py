a1b940ddb6a967d6cbe6f689dff8dc8e34ea343c
8140se <alin.hincu@edu.uah.es>
2023-02-23 20:33:35 +0100

    Text Gamu

---
+++ lab06-adventure/lab06-adventure.py
@@ -0,0 +1,25 @@
+class Room:
+    """Room"""
+    def __init__(self, description, north, east, south, west):
+        self.description = description
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+
+def main():
+    current_room = 0
+    room_list = []
+    room0 = Room("You find yourself in an open space, you are blinded by the pure white lights from the ceiling. The room seems to continue up ahead.",
+                1,
+                None,
+                None,
+                None)
+
+    done = False
+    room_list.append(room0)
+    while not done:
+        print("")
+        print(room_list[current_room].description)
+
+main()
